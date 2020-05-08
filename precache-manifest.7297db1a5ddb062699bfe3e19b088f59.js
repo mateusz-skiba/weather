f926d1faa6f2d6f10f24789328c061a2e2667694
Skibol <mateusz.skiba14@gmail.com>
2020-05-08 09:18:28 +0200

    Updates

---
+++ precache-manifest.7297db1a5ddb062699bfe3e19b088f59.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b25c2c9c6de65c81558862daa6c8c790",
+    "url": "/weather/index.html"
+  },
+  {
+    "revision": "ca381209c080fce4cb37",
+    "url": "/weather/static/css/main.466a08b3.chunk.css"
+  },
+  {
+    "revision": "c8fff2b3a128c3958499",
+    "url": "/weather/static/js/2.322dd519.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather/static/js/2.322dd519.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca381209c080fce4cb37",
+    "url": "/weather/static/js/main.372cc030.chunk.js"
+  },
+  {
+    "revision": "afec1267bcb00ee24eb9",
+    "url": "/weather/static/js/runtime-main.9352a2ef.js"
+  },
+  {
+    "revision": "7ddb9d745b71e0d8f332acb8c29de394",
+    "url": "/weather/static/media/background.7ddb9d74.png"
+  }
+]);
